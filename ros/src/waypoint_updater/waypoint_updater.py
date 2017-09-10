e04199413c04e9fcebf73e4a6b011bd5438e4155
Tevine <f_hall@mail.de>
2017-09-10 19:59:01 +0200

    Number 3

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -5,6 +5,8 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 
+import tf
+
 import math
 
 '''
@@ -28,10 +30,11 @@ LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this n
 class WaypointUpdater(object):
     def __init__(self):
         self.wp = []
-        self.test = 0
-        self.test2 = 0
-        self.pub_wp = []
+        self.count_waypoint = 0
+        self.waypoints = 0
+        self.pub_wp = Lane()
         self.current_pose = np.array([0.0,0.0,0.0])
+        self.current_orientation = np.array([0.0, 0.0, 0.0, 0.0])
         self.current_dir = np.array([0.0,0.0,0.0,0.0])
         self.numpy_list = np.zeros((1, 3))
         self.distance_array = np.zeros(1)
@@ -47,17 +50,18 @@ class WaypointUpdater(object):
         while not rospy.is_shutdown():
             self.distance_array = np.zeros(self.numpy_list.shape[0])
             for i in range(self.numpy_list.shape[0]):
-                self.distance_array[i]=np.linalg.norm(self.current_pose-self.numpy_list[i])
+                self.distance_array[i] = np.linalg.norm(self.current_pose - self.numpy_list[i])
             if (self.numpy_list.shape[0] > 1):
-                rospy.loginfo(self.test)
-                rospy.loginfo(len(self.test2.waypoints))
+                rospy.loginfo(self.count_waypoint)
+                rospy.loginfo(len(self.waypoints.waypoints))
                 minimum = np.argmin(self.distance_array)
-                self.pub_wp = []
+                minimum = self.transform_and_ahead(self.waypoints.waypoints[minimum], minimum)
+                self.pub_wp = Lane()
                 for i in range(LOOKAHEAD_WPS):
-                    self.pub_wp.append(self.test2.waypoints[minimum + i])
-                rospy.loginfo(self.pub_wp[0].pose.pose.position)
+                    self.pub_wp.waypoints.append(self.waypoints.waypoints[(minimum + i)% len(self.waypoints.waypoints)])
+                #rospy.loginfo(self.pub_wp[0].pose.pose.position)
                 rospy.loginfo(self.current_pose)
-            rospy.loginfo("test")
+                self.final_waypoints_pub.publish(self.pub_wp)
             rate.sleep()
 
         # TODO: Add other member variables you need below
@@ -69,10 +73,10 @@ class WaypointUpdater(object):
         self.current_pose[1] = msg.pose.position.y
         self.current_pose[2] = msg.pose.position.z
 
-        #self.current_dir[0] = msg.pose.orientation.x
-        #self.current_dir[1] = msg.pose.orientation.y
-        #self.current_dir[2] = msg.pose.orientation.z
-        #self.current_dir[3] = msg.pose.orientation.w
+        self.current_orientation[0] = msg.pose.orientation.x
+        self.current_orientation[1] = msg.pose.orientation.y
+        self.current_orientation[2] = msg.pose.orientation.z
+        self.current_orientation[3] = msg.pose.orientation.w
 
         # TODO: Implement
 
@@ -83,18 +87,11 @@ class WaypointUpdater(object):
             self.numpy_list[i][0] = waypoints.waypoints[i].pose.pose.position.x
             self.numpy_list[i][1] = waypoints.waypoints[i].pose.pose.position.y
             self.numpy_list[i][2] = waypoints.waypoints[i].pose.pose.position.z
-        self.test = count_waypoint
-        self.test2 = waypoints
-        #rospy.loginfo(self.numpy_list[523])
-        #ads = ()
-        #ads = waypoints
-        #for i in range(len(ads)):
-        #    i = i+1
-        #rospy.loginfo(i)
-        #rospy.loginfo(waypoints)
-        #rospy.loginfo("________above____________")
+        self.count_waypoint = count_waypoint
+        self.waypoints = waypoints
+
         # TODO: Implement
-        #pass
+        # pass
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -103,6 +100,24 @@ class WaypointUpdater(object):
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
+
+    def transform_and_ahead(self, waypoint, id):
+        wx = waypoint.pose.pose.position.x
+        wy = waypoint.pose.pose.position.y
+
+        rospy.loginfo(self.current_orientation[0])
+        rospy.loginfo("---------------------")
+        _,_,yaw = tf.transformations.euler_from_quaternion([self.current_orientation[0],self.current_orientation[1],self.current_orientation[2], self.current_orientation[3]])
+
+        dx = wx - self.current_pose[0]
+        dy = wy - self.current_pose[1]
+
+        wx_local = math.cos(-yaw)*dx - math.sin(-yaw) * dy
+
+        if wx_local > 0.0:
+            return id
+        else:
+            return id+1
 
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
